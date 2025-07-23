4c774fadb3f8ba895441cf3d3e91719cba058fda
rolindaz <rolinda.zamboni@gmail.com>
2025-07-23 14:42:24 +0200

    refactor App component layout and improve form structure for post creation

--- src/App.jsx
+++ src/App.jsx
@@ -4,69 +4,71 @@ function App() {
 
   return (
     <>
-      {/* Text input for Author */}
-      <div class="mb-3">
-        <label for="author" class="form-label">
-          Author
-        </label>
-        <input
-          type="text"
-          class="form-control"
-          value=''
-          name="author"
-          id="author"
-          aria-describedby="helpId"
-          placeholder="Type the author name here"
-        />
-        <small id="helpId" class="form-text text-muted">
-          Author name
-        </small>
-      </div>
-      {/* Text input for Title */}
-      <div class="mb-3">
-        <label for="Title" class="form-label">
-          Title
-        </label>
-        <input
-          type="text"
-          class="form-control"
-          value=''
-          name="title"
-          id="title"
-          aria-describedby="helpId"
-          placeholder="Type the title of the post here"
-        />
-        <small id="helpId" class="form-text text-muted">
-          Post Title
-        </small>
-      </div>
-      {/* Textarea input for Body */}
-      <div class="mb-3">
-        <label for="body" class="form-label"></label>
-        <textarea class="form-control"
-          name="body"
-          value=''
-          id="body"
-          rows="3"
-          placeholder='Enter the content of your post here'
-        >
-        </textarea>
-        {/* Checkbox input for Public */}
-        <div class="form-check">
-          <input
-            class="form-check-input"
-            type="checkbox"
-            name='public'
-            id="public"
-          />
-          <label class="form-check-label" for="public">
-            Publish this post
-          </label>
+      <div className="container mt-5">
+        <div className="card p-4">
+          <form>
+            {/* Text input for Author */}
+            <div className="mb-3">
+              <label htmlFor="author" className="form-label">
+                Author
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                value=''
+                name="author"
+                id="author"
+                aria-describedby="helpId"
+                placeholder="Type the name of the author here"
+              />
+            </div>
+            {/* Text input for Title */}
+            <div className="mb-3">
+              <label htmlFor="Title" className="form-label">
+                Title
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                value=''
+                name="title"
+                id="title"
+                aria-describedby="helpId"
+                placeholder="Type the title of the post here"
+              />
+            </div>
+            {/* Textarea input for Body */}
+            <div className="mb-3">
+              <label htmlFor="body" className="form-label">
+                Post Content
+              </label>
+              <textarea className="form-control"
+                name="body"
+                value=''
+                id="body"
+                rows="3"
+                placeholder='Enter the content of your post here'
+              >
+              </textarea>
+            </div>
+            {/* Checkbox input for Public */}
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                name='public'
+                id="public"
+              />
+              <label className="form-check-label" htmlFor="public">
+                Publish this post?
+              </label>
+            </div>
+            <button className='btn btn-primary mt-3' type="submit">
+              Add New Post
+            </button>
+          </form>
         </div>
-
       </div>
-
-
     </>
   )
 }
