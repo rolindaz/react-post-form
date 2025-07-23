528b92d52c6dd045e746ba06a8edfb68b2dfd558
rolindaz <rolinda.zamboni@gmail.com>
2025-07-23 14:29:42 +0200

    add text input for author in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,24 @@ function App() {
 
   return (
     <>
-
+      {/* Text input for Author */}
+      <div class="mb-3">
+        <label for="author" class="form-label">
+          Autore
+        </label>
+        <input
+          type="text"
+          class="form-control"
+          value=''
+          name="author"
+          id="author"
+          aria-describedby="helpId"
+          placeholder="Type the author name here"
+        />
+        <small id="helpId" class="form-text text-muted">
+          Author name
+        </small>
+      </div>
 
     </>
   )
